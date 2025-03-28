d905494d711caa07ddce2b434bd7bc2fbd8a2b3a
egoldman1 <ethangoldman56@gmail.com>
2025-03-28 19:36:38 +1100

    create login and register page, complete User model class

--- assignment1/src/main/java/model/User.java
+++ assignment1/src/main/java/model/User.java
@@ -1,5 +1,19 @@
 package model;
 
 public class User {
+    private Integer userID;
+    private String fullName;
+    private Integer phoneNumber;
+    private String email;
+    private String password;
+    private String address1;
+    private String address2;
+    private String suburb;
+    private Integer postcode;
+    private String state; 
+    private Integer cardNumber;
+    private Integer cardExpiry;
+    private Integer cardCVV;
+
     
 }
