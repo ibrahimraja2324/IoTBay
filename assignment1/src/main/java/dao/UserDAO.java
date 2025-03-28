93cc69dd644702b9353de36a7cdb364a3b7c6c49
egoldman1 <ethangoldman56@gmail.com>
2025-03-28 20:06:59 +1100

    a lot of stuff implemented, just read the code lil bro

---
+++ assignment1/src/main/java/dao/UserDAO.java
@@ -0,0 +1,37 @@
+package dao;
+
+import iotbay.model.User;
+import iotbay.util.DBConnector;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAO {
+
+    // Validate a user login by checking email and password
+    public User validateUser(String email, String password) {
+        User user = null;
+        String sql = "SELECT * FROM User WHERE EmailAddress = ? AND Password = ?";
+        try (Connection conn = DBConnector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            
+            stmt.setString(1, email);
+            stmt.setString(2, password);  // In production, use hashed passwords!
+            
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                user = new User();
+                user.setUserID(rs.getInt("UserID"));
+                user.setName(rs.getString("Name"));
+                user.setEmailAddress(rs.getString("EmailAddress"));
+                // set other fields as needed...
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();  // Handle exceptions appropriately
+        }
+        return user;
+    }
+    
+    // You can add more methods to register new users, update user info, etc.
+}
--- assignment1/src/main/webapp/login.jsp
+++ assignment1/src/main/webapp/login.jsp
@@ -1,14 +1,18 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <title>Login - IoTBay</title>
-        <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <form class="login-box" action="LoginServlet" method="post">
         <h2>Welcome Back</h2>
+        <c:if test="${not empty errorMessage}">
+            <p style="color: red;">${errorMessage}</p>
+        </c:if>
         <input type="text" name="email" placeholder="Email" required>
         <input type="password" name="password" placeholder="Password" required>
         <input type="submit" value="Login">
