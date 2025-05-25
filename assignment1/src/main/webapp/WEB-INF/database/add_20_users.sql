252e51afb98c73faf37341d023eb97e247f073b4
egoldman2 <ethan.goldman@student.uts.edu.au>
2025-05-25 19:46:15 +1000

    added 20 users 🥀🥀🥀

---
+++ assignment1/src/main/webapp/WEB-INF/database/add_20_users.sql
@@ -0,0 +1,59 @@
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('amanda.miller@business.net', 'user696', 'Amanda', 'Miller', '3425346469', True, '2025-05-08 15:37:03', '2025-05-08 15:37:03', 'USER');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('john.jones@office.org', 'staff402', 'John', 'Jones', '6831086810', True, '2025-05-14 00:29:03', '2025-05-14 00:29:03', 'USER');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('david.wilson@company.com', 'admin371', 'David', 'Wilson', '6713561753', True, '2025-05-11 15:55:03', '2025-05-17 15:55:03', 'STAFF');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('david.brown@business.net', 'staff603', 'David', 'Brown', '4153458103', True, '2025-04-29 18:36:03', '2025-04-29 18:36:03', 'ADMIN');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('ashley.jones@office.org', 'staff597', 'Ashley', 'Jones', '3470868996', True, '2025-04-28 02:05:03', '2025-04-28 02:05:03', 'STAFF');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('michelle.williams@company.com', 'staff306', 'Michelle', 'Williams', '1383893364', True, '2025-05-02 21:49:03', '2025-05-02 21:49:03', 'ADMIN');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('john.lopez@acc.com', 'pass633', 'John', 'Lopez', '9965287461', True, '2025-05-15 07:18:03', '2025-05-15 07:18:03', 'USER');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('james.thomas@office.org', 'pass949', 'James', 'Thomas', '3699113215', True, '2025-05-12 05:51:03', '2025-05-12 05:51:03', 'ADMIN');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('james.brown@business.net', 'user839', 'James', 'Brown', '5305531153', True, '2025-05-14 04:33:03', '2025-05-15 04:33:03', 'USER');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('james.miller@company.com', 'pass914', 'James', 'Miller', '7028204827', True, '2025-05-24 02:01:03', '2025-05-30 02:01:03', 'ADMIN');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('jessica.gonzalez@acc.com', 'user619', 'Jessica', 'Gonzalez', '8937523834', True, '2025-04-26 15:44:03', '2025-04-26 15:44:03', 'ADMIN');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('robert.hernandez@company.com', 'pass938', 'Robert', 'Hernandez', '3039503786', True, '2025-05-15 12:52:03', '2025-05-21 12:52:03', 'ADMIN');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('christopher.williams@acc.com', 'pass208', 'Christopher', 'Williams', '9600408249', True, '2025-05-08 23:39:03', '2025-05-15 23:39:03', 'USER');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('christopher.martinez@office.org', 'staff698', 'Christopher', 'Martinez', '1722733353', True, '2025-05-25 01:52:03', '2025-05-29 01:52:03', 'ADMIN');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('james.williams@company.com', 'user403', 'James', 'Williams', '6830967104', True, '2025-05-04 00:49:03', '2025-05-07 00:49:03', 'ADMIN');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('emily.smith@company.com', 'staff857', 'Emily', 'Smith', '4551126160', True, '2025-05-05 03:24:03', '2025-05-05 03:24:03', 'STAFF');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('james.williams@business.net', 'admin827', 'James', 'Williams', '4570904037', True, '2025-05-08 07:35:03', '2025-05-10 07:35:03', 'ADMIN');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('lisa.johnson@acc.com', 'admin102', 'Lisa', 'Johnson', '7501453205', True, '2025-05-12 16:01:03', '2025-05-20 16:01:03', 'ADMIN');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('daniel.martinez@company.com', 'admin205', 'Daniel', 'Martinez', '1122329488', True, '2025-05-19 02:27:03', '2025-05-19 02:27:03', 'ADMIN');
+
+INSERT INTO "Users" (Email, Password, FirstName, LastName, PhoneNumber, IsActive, CreatedAt, UpdatedAt, Role) VALUES
+('jessica.lopez@company.com', 'admin950', 'Jessica', 'Lopez', '9124125285', True, '2025-05-08 15:15:03', '2025-05-14 15:15:03', 'STAFF');
